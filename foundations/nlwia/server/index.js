7f30b71533c5ce7d27d9cad65a39a28472bafd9b
Roberto Zanin <raszanin@gmail.com>
2023-09-13 11:47:29 -0300

    day02:backend

---
+++ foundations/nlwia/server/index.js
@@ -0,0 +1,17 @@
+import cors from "cors";
+import express from "express";
+import { download } from "./download.js";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/summary/:id", (request, response) => {
+  const id = request.params.id;
+
+  download(id);
+
+  return response.json({ message: "Download do vídeo realizado com sucesso!" });
+});
+
+app.listen(3333, () => console.log("Server is running on port 3333"));
